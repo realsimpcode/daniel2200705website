03a122b733549b145a8bdac30124f6c1e72d1de4
daniel2200705 <80725014+daniel2200705@users.noreply.github.com>
2021-04-26 14:55:17 +0900

    Update daniel2200705.php

--- daniel2200705.php
+++ daniel2200705.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Daniel2200705 login page</title>
+    <link rel="stylesheet" href="login1.css">
+<link rel="preconnect" href="https;//fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+    <span class="team"><a href="/teamlogin.php" id="php">TeamMoonWed <span class="log">login</span></a></span>
+    </header>
+    <dr>
+    <div class="login1">please login.</div>
 
+    <span id="t">made by daniel2200705</span>
+     </body>
+</html>
